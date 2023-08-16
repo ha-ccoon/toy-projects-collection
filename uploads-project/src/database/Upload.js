2ad505048f9716f48bbdc22922c6316f1ff1f842
haccoon <haccoon@sqkcloud.com>
2023-08-17 08:21:23 +0900

    [DOC] 폴더 이름 변경

---
+++ uploads-project/src/database/Upload.js
@@ -0,0 +1,42 @@
+// const { Sequelize, DataTypes, Model } = require('sequelize');
+// const sequelize = require('../config/database.js');
+
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database.js';
+
+class Upload extends Model {}
+
+Upload.init(
+  {
+    upload_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    path: {
+      type: DataTypes.BLOB('long'),
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    underscored: true,
+    tableName: 'upload',
+    modelName: 'upload',
+    charset: 'utf8',
+    collate: 'utf8_general_ci',
+  }
+);
+
+// Upload.sync({ alter: true })
+//   .then(() => {
+//     console.log('Upload table has been updated');
+//   })
+//   .catch((err) => {
+//     console.error('Upload table:', err);
+//   });
+
+export default Upload;
