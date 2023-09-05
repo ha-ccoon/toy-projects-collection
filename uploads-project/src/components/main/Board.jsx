c1326c00e2df646614a81f54d1ea251112fed178
haccoon <haccoon@sqkcloud.com>
2023-09-05 09:48:03 +0900

    [uploads] i18n 언어팩 구현중

--- uploads-project/src/components/main/Board.jsx
+++ uploads-project/src/components/main/Board.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import * as S from './Board.styled';
+
+import { useTranslation } from 'next-i18next';
 
 function Board(props) {
+  const { t } = useTranslation('common');
+
   return (
     <div>
-      <h1>공지사항</h1>
-      
+      <h1>{t('test')}</h1>
+      <S.LayoutDiv>
+        <S.BoardH1>공지사항</S.BoardH1>
+      </S.LayoutDiv>
     </div>
   );
 }
--- uploads-project/src/pages/index.jsx
+++ uploads-project/src/pages/index.jsx
@@ -3,9 +3,15 @@ import Main from '@/components/main/Main';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+import { useTranslation } from 'next-i18next';
+
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
 function index(props) {
+  const { t } = useTranslation('common');
   return (
     <div>
+      <p>{t('serverSideTest')}</p>
       <Header />
       <Main />
       <Footer />
