d6df3cf2daa686a8883b922f61cf007f41c98661
minha sohn <codeminhasohn@gmail.com>
2023-10-15 22:39:58 +0900

    [nest/PROG 게시물 생성 로직 구현

--- nestjs-board/src/boards/boards.controller.ts
+++ nestjs-board/src/boards/boards.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  /**
+   *
+   * @param title
+   * @param description
+   * @returns: board (return only board itself so the type is just "Board")
+   */
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
+}
