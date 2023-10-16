3fa4621b997b5bbcc6bd376c8f7cb17e6f206ab7
minha sohn <codeminhasohn@gmail.com>
2023-10-17 08:26:23 +0900

    [nest/PROG] pg 연결 및 entity 설정

---
+++ nestjs-board/src/boards/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '121314',
+  database: 'board-app',
+  entities: [__dirname + '/../**/*,entity.{js,ts}'],
+  synchronize: true,
+};
+
+\
